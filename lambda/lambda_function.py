e57e472f8b8e47a89c1bc907e8549c0a5d82cd9b
joaogaioso7 <joaovitorgaioso7@gmail.com>
2025-08-24 23:57:04 -0300

    Change lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,8 +1,12 @@
 import json
 
 def lambda_handler(event, context):
+
+    mensagem = "Hello from Lambda created by Terraform CI/CD"
+    print(mensagem)
     
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda created by Terraform!')
     }
+
